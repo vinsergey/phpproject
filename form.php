80be654fbb1158e8769d4ddc3b8fcaa20d9dd209
vinsergey <vinsergey81@gmail.com>
2018-04-13 20:13:53 +0300

    form added

---
+++ form.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Form</title>
+</head>
+<body>
+<div class="container" style="margin-top: 50px">
+<form method="post" action="post.php?id=11">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
-error_reporting(-1);
+//error_reporting(-1);
+//
+//// То же, что и error_reporting(E_ALL);
+//ini_set('error_reporting', E_ALL);
+//
+//$array = [1, 2, 3];
+//
+//$array_primitive = array('hi', 'number', 6);
+//$as_array = ['first' => 1, 6 => 'first', 3];
+//
+//echo $as_array['first'];
 
-// То же, что и error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-
-
-$array = [1, 2, 3];
-
-$array_primitive = array('hi', 'number', 6);
-$as_array = ['first' => 1, 6 => 'first', 3];
-
-echo $as_array['first'];
+echo "index.php";
