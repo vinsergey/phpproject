104b1956be8e996e0bf4f5959c5423976a51c459
vinsergey <vinsergey81@gmail.com>
2018-04-13 19:03:11 +0300

    add treat les2

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
+error_reporting(-1);
+
+// То же, что и error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+
 $array = [1, 2, 3];
-$array_primitive = $array('hi', 'number', 6);
+
+$array_primitive = array('hi', 'number', 6);
 $as_array = ['first' => 1, 6 => 'first', 3];
 
 echo $as_array['first'];
